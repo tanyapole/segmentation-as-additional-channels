4be9425492839ca84f94de86e5c05035b95ce68b
oem <xanilegendx@gmail.com>
2020-02-28 09:42:35 +0300

    modification prepare.sh

--- Bash_scripts/prepare.sh
+++ Bash_scripts/prepare.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python my_preprocesss.py --impath $1 --maskpath $2 --svpath $3 --size $4 --jobs $5
+python Preprocessing/my_preprocess.py --impath ./$1 --maskpath ./$2 --svpath ./$3 --size $4 --jobs $5
