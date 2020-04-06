314279f7b2213a2b1b0d9806c7bc78741fb6f170
Admin <xanilegendx@gmail.com>
2020-04-06 14:37:40 +0300

    preparing bash and related

--- Bash_scripts/run_exp_resume_general.sh
+++ Bash_scripts/run_exp_resume_general.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python my_main.py --model resnet50 --N 10 --lr 0.0001 --batch_size 100 --mask_use --resume --augment_list hflip vflip affine adjust_brightness adjust_saturation --attribute attribute_globules attribute_milia_like_cyst attribute_negative_network attribute_pigment_network attribute_streaks --cell_size 14 28 56 --prob 0.2 0.8  --n_epochs 400 --workers 5 --image_path ./$1 --model_path ./$2
+python my_main.py --model resnet50 --N 10 --lr 0.0001 --batch_size 100 --mask_use --resume --augment_list hflip vflip affine adjust_brightness adjust_saturation --attribute attribute_milia_like_cyst attribute_pigment_network --cell_size 14 28 56 --prob 0.2 0.8  --n_epochs 200 --workers 5 --image_path ./$1 --model_path ./$2
