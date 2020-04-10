a9757b942b1047e05cc451a712076a5ef9faa030
Admin <xanilegendx@gmail.com>
2020-04-10 16:17:50 +0300

    checking

--- metrics.py
+++ metrics.py
@@ -1,37 +1,43 @@
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import multilabel_confusion_matrix
+import numpy as np
 
 
 class Metrics:
 
-    def __init__(self):
-        self.tp = 0
-        self.fp = 0
-        self.fn = 0
-        self.tn = 0
+    def __init__(self, args):
+        self.conf_matrix = np.zeros([args.attribute, 2, 2])
         self.loss = []
         self.bce_loss = []
         self.std_loss = []
 
     def update(self, y_true, y_pred, loss, bce_loss, std_loss):
-
-        tn, fp, fn, tp = confusion_matrix(y_true, y_pred, labels=[0, 1]).ravel()
-        print(tn, fp, fn, tp)
-        self.tp += tp
-        self.fp += fp
-        self.fn += fn
-        self.tn += tn
+        self.conf_matrix += multilabel_confusion_matrix(y_true, y_pred, labels=[0,1])
         self.loss.append(loss.item())
         self.bce_loss.append(bce_loss.item())
         self.std_loss.append(std_loss.item())
 
     def compute(self, ep, epoch_time):
-        acc = (self.tp + self.tn) / (self.tp + self.tn + self.fp + self.fn)  # TP + TN / (TP + TN + FP + FN)
-        prec = self.tp / (self.tp + self.fp)  # TP / (TP + FP)
-        rec = self.tp / (self.tp + self.fn)  # TP / (TP + FN)
-        f1 = 2 * prec * rec / (rec + prec)  # 2 * PREC * REC / (PREC + REC)
+
+        acc_l = []
+        prec_l =[]
+        rec_l = []
+        f1_l  = []
+        for cm in self.conf_matrix:
+            tn, fp, fn, tp = cm.ravel()
+            acc_l.append((tp + tn) / (tp + tn + fp + fn))          # TP+TN/(TP+TN+FP+FN)
+            p = tp / (tp + fp)                                     # TP   /(TP+FP)
+            prec_l.append(p)
+            r = tp / (tp + fn)                                     # TP   /(TP+FN)
+            rec_l.append(r)
+            f1_l.append(2 * p * r / (p + r))                       # 2*PREC*REC/(PREC+REC)
+        acc  = sum(acc_l) / len(acc_l)
+        prec = sum(prec_l)/ len(prec_l)
+        rec  = sum(rec_l) / len(rec_l)
+        f1   = sum(f1_l)  / len(f1_l)
         loss = sum(self.loss) / len(self.loss)
         bce_loss = sum(self.bce_loss) / len(self.bce_loss)
         std_loss = sum(self.std_loss) / len(self.std_loss)
+
         return {
                     'epoch': ep,
                     'loss': loss,
@@ -39,16 +45,17 @@ class Metrics:
                     'std_loss': std_loss,
                     'epoch_time': epoch_time,
                     'accuracy': acc,
+                    'accuracy_labels': acc_l,
                     'precision': prec,
+                    'precision_labels': prec_l,
                     'recall' : rec,
-                    'f1_score': f1
+                    'recall_labels': rec_l,
+                    'f1_score': f1,
+                    'f1_score_labels': f1_l
                 }
 
-    def reset(self):
-        self.tp = 0
-        self.fp = 0
-        self.fn = 0
-        self.tn = 0
+    def reset(self, args):
+        self.conf_matrix = np.zeros([args.attribute, 2, 2])
         self.loss = []
         self.bce_loss = []
         self.std_loss = []
