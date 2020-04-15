c1542715dbeb0941d140f8ad1aec7cb84f05dfb5
Admin <xanilegendx@gmail.com>
2020-04-15 22:10:23 +0300

    vgg16 repeat experiment modification

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,10 +1,8 @@
 import json
-
 import h5py
 import torch
 import pandas as pd
 import os
-import sys
 
 
 def load_image(file_name):
@@ -14,9 +12,8 @@ def load_image(file_name):
 
 
 def print_save_results(args, results, root, i, time):
-    print('Использование масок на трейне {} Заморозка {}, шаг обучения {}, номер эксперимента {} размер квадрата {}'
-          'вероятность {}'.format(args.mask_use, args.freezing, args.lr, args.N, args.cell_size, args.prob))
-    root.joinpath('params {}_num {} .json'.format(time, i)).write_text(
+    print('Модель {}, заморозка {}, шаг обучения {}, номер эксперимента {}'.format(args.model, args.freezing, args.lr, args.N))
+    root.joinpath('params_{}_num_{}.json'.format(time, i)).write_text(
         json.dumps(vars(args), indent=True, sort_keys=True))
     path = 'Results/{}'.format(time)
     name = 'results.csv'
