2ced5672eecb584597bdac29447106cf1e2fbf96
Admin <xanilegendx@gmail.com>
2020-04-16 22:36:29 +0300

    vgg16 repeat bash modification

--- Bash_scripts/run_exp_base_train_all_class.sh
+++ Bash_scripts/run_exp_base_train_all_class.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python my_main.py --model resnet50 --N 10 --lr 0.0001 --batch_size 100 --augment_list hflip vflip affine adjust_brightness adjust_saturation --attribute attribute_globules attribute_milia_like_cyst attribute_negative_network attribute_pigment_network attribute_streaks  --n_epochs 400 --workers 5 --image_path ./$1 --model_path ./$2
+python my_main.py --model resnet50 --N 10 --lr 0.0001 --batch_size 5 --attribute attribute_globules attribute_milia_like_cyst attribute_negative_network attribute_pigment_network attribute_streaks  --n_epochs 100 --workers 5 --optimizer adam --image_path ./$1
