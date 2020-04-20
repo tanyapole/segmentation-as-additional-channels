717055d66e7a3b7072b755019b46f7028da39a08
Admin <xanilegendx@gmail.com>
2020-04-20 17:56:51 +0300

    vgg16 experiment num fix

--- Bash_scripts/run_exp_base_train_2_class.sh
+++ Bash_scripts/run_exp_base_train_2_class.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python my_main.py --model vgg16 --pretrained --N 1 --lr 0.0001 --batch_size 2 --attribute attribute_milia_like_cyst attribute_pigment_network --n_epochs 1 --workers 2 --image_path D:/Data/h5/ --optimizer adam
+python my_main.py --model vgg16 --pretrained --N 5 --lr 0.0001 --batch_size 10 --attribute attribute_milia_like_cyst attribute_pigment_network --n_epochs 200 --workers 5 --image_path D:/Data/h5/ --optimizer adam
