06029bea29c7b0d516286d8e79195d2238d243ff
Admin <xanilegendx@gmail.com>
2020-04-30 03:21:47 +0300

    aux mse

--- models.py
+++ models.py
@@ -111,7 +111,7 @@ class ResNet(nn.Module):
         x = torch.flatten(x, 1)
         x = self.fc(x)
 
-        return x, x1
+        return x, x2
 
     def forward(self, x):
         return self._forward_impl(x)
