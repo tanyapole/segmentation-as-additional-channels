48243c7f1e1c61847a517c20488a5d3915faa882
Admin <xanilegendx@gmail.com>
2020-05-08 21:30:14 +0300

    segm pretrain

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torchvision import models
 
-from Utils.constants import YNET
+from Utils.constants import YNET, PRETRAIN
 
 
 class ConvBlock(nn.Module):
@@ -62,14 +62,14 @@ class ResYNet(nn.Module):
         self.down5 = base_model.layer3
         self.down6 = base_model.layer4
 
-        self.conv1x1 = nn.Conv2d(2048, 512, kernel_size=1)
-        self.bn = nn.BatchNorm2d(512)
+        self.conv1x1 = nn.Conv2d(512, 256, kernel_size=1)
+        self.bn = nn.BatchNorm2d(256)
         self.relu = nn.ReLU()
         self.avgpool = base_model.avgpool
-        self.fc1 = nn.Linear(512, 512, bias=False)
-        self.bn1 = nn.BatchNorm1d(512)
+        self.fc1 = nn.Linear(256, 256, bias=False)
+        self.bn1 = nn.BatchNorm1d(256)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(512, n_class, bias=False)
+        self.fc2 = nn.Linear(256, n_class, bias=False)
 
         self.MiddleBridge = nn.Sequential(*[ConvBlock(2048, 2048),
                                             ConvBlock(2048, 2048)])
@@ -98,13 +98,13 @@ class ResYNet(nn.Module):
         # z = self.Bridge2(b)
 
         z = self.up1((b, x5))  # -> 14x14x1024
-        z = self.up2((z, x4))  # -> 28x28x512
+        z1 = self.up2((z, x4))  # -> 28x28x512
         z = self.up3((z, x3))  # -> 56x56x256
         z = self.up4((z, x1))  # -> 112x112x128
         z = self.up5((z, x))   # -> 224x224x64
         z = self.conv_segm(z)  # -> 224x224xn
 
-        x = self.conv1x1(b)   # classification
+        x = self.conv1x1(z1)   # classification
         x = self.bn(x)
         x = self.relu(x)
         x = self.avgpool(x)
@@ -117,12 +117,63 @@ class ResYNet(nn.Module):
         return x, z
 
 
+class Unet(nn.Module):
+
+    def __init__(self, pretrained: bool, n_class: int):
+        super().__init__()
+
+        base_model = models.resnet50(pretrained=pretrained)
+        base_model.fc = nn.Linear(2048, n_class)
+
+        self.down1 = nn.Sequential(*[base_model.conv1,
+                                     base_model.bn1,
+                                     base_model.relu])
+        self.down2 = base_model.maxpool
+        self.down3 = base_model.layer1
+        self.down4 = base_model.layer2
+        self.down5 = base_model.layer3
+        self.down6 = base_model.layer4
+
+        self.MiddleBridge = nn.Sequential(*[ConvBlock(2048, 2048),
+                                            ConvBlock(2048, 2048)])
+
+        self.up1 = UnetUpBlock(2048, 1024)
+        self.up2 = UnetUpBlock(1024, 512)
+        self.up3 = UnetUpBlock(512, 256)
+        self.up4 = UnetUpBlock(in_channels=128 + 64, out_channels=128,
+                               up_conv_in_channels=256, up_conv_out_channels=128)
+        self.up5 = UnetUpBlock(in_channels=64 + 3, out_channels=64,
+                               up_conv_in_channels=128, up_conv_out_channels=64)
+        self.conv_segm = nn.Conv2d(64, n_class, 1)
+
+    def forward(self, x):
+
+        x1 = self.down1(x)  # -> 112x112x64
+        x2 = self.down2(x1)  # -> 56x56x64
+        x3 = self.down3(x2)  # -> 56x56x256
+        x4 = self.down4(x3)  # -> 28x28x512
+        x5 = self.down5(x4)  # -> 14x14x1024
+        x6 = self.down6(x5)  # -> 7x7x2048
+
+        b = self.MiddleBridge(x6)
+
+        z = self.up1((b, x5))  # -> 14x14x1024
+        z = self.up2((z, x4))  # -> 28x28x512
+        z = self.up3((z, x3))  # -> 56x56x256
+        z = self.up4((z, x1))  # -> 112x112x128
+        z = self.up5((z, x))  # -> 224x224x64
+        z = self.conv_segm(z)  # -> 224x224xn
+
+        return x, z
+
+
 def create_model(args, train_type):
 
     if train_type == YNET:
         model = ResYNet(args.pretrained, len(args.attribute), args.model_path, args.N)
+    elif train_type == PRETRAIN:
+        model = Unet(args.pretrained, len(args.attribute))
     else:
         model = models.resnet50(pretrained=args.pretrained)
         model.fc = nn.Linear(2048, len(args.attribute))
-
     return model
