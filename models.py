2650dcf445e5267e88a918ad0ce0605457bbf791
Admin <xanilegendx@gmail.com>
2020-05-15 14:50:56 +0300

    5 class strong right run

--- models.py
+++ models.py
@@ -98,13 +98,13 @@ class ResYNet(nn.Module):
         # z = self.Bridge2(b)
 
         z = self.up1((b, x5))  # -> 14x14x1024
-        z = self.up2((z, x4))  # -> 28x28x512
-        z = self.up3((z, x3))  # -> 56x56x256
+        z1 = self.up2((z, x4))  # -> 28x28x512
+        z = self.up3((z1, x3))  # -> 56x56x256
         z = self.up4((z, x1))  # -> 112x112x128
         z = self.up5((z, x))   # -> 224x224x64
         z = self.conv_segm(z)  # -> 224x224xn
 
-        x = self.clsf(x4)   # classification
+        x = self.clsf(z1)   # classification
 
         return x, z
 
