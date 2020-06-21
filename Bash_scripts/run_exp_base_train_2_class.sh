5ae47b8d2d566688c8b7cc502bf17444a2a9a6bf
Admin <xanilegendx@gmail.com>
2020-06-21 21:33:27 +0300

    refactoring

--- Bash_scripts/run_exp_base_train_2_class.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python main.py --model vgg16 --pretrained --N 5 --lr 0.0001 --batch_size 10 --attribute attribute_milia_like_cyst attribute_pigment_network --n_epochs 200 --workers 5 --image_path D:/Data/h5/ --optimizer adam
